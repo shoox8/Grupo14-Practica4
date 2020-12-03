a9706dcca4850117442b54e9162d34fc74469726
cralbert79 <cralbert79@gmail.com>
2020-12-03 20:12:48 +0100

    Menú para Login

---
+++ src/main.cc
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+
+int main() {
+	int option;
+	char user, password;
+	cout<<"Seleccione la opcion que desee realizar:"<<endl;
+	cout<<"1.Login"<<endl;
+	cout<<"2.Salir"<<endl;
+	cin<<option;
+		if(option==1){
+			cout<<"Introduzca su usuario:"<<endl;
+			cin>>user;
+			cout<<"Introduzca su contraseña:"<<endl;
+			cin>>password;
+		}
+		else {
+			cout<<"Ha salido del programa."<<endl;
+			return 0;
+		}
+}
