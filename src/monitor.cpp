6c6ab9f03e8bb141cea9c368c9120e6284c5363d
alejandrojgb94 <55994125+alejandrojgb94@users.noreply.github.com>
2020-12-13 22:28:42 +0100

    Update monitor.cpp

--- src/monitor.cpp
+++ src/monitor.cpp
@@ -18,22 +18,22 @@ void Monitor::mostrarMonitor()
 		f.close();
 	}
 
-	/*cout << "Nombre" << _nombre << endl;
+	/*cout << "Nombre: " << _nombre << endl;
 	cout << "DNI: " << _dni << endl;
 	cout << "Telefono: " << _telefono << endl;
 	cout << "ruta: " << _ruta << endl;*/
 }
 
-void registrarMonitor(string nombre, string dni, int telefono, string ruta)
+void Monitor::registrarMonitor(string nombre, string dni, int telefono, string ruta)
 {
-	cout << "Introduza el nombre del monitor que desea registrar" << endl;
+	/*cout << "Introduza el nombre del monitor que desea registrar" << endl;
 	cin >> nombre;
 	cout << "Introduzca el dni del monitor" << endl;
 	cin >> dni;
 	cout << "Introduzca el teléfono del monitor" << endl;
 	cin >> telefono;
 	cout << "Introduzca la ruta que se le ha asociado al monitor" << endl;
-	cin >> ruta;
+	cin >> ruta;*/
 
 	ofstream f;
 	f.open("monitor.txt");
