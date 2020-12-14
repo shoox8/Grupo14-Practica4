97d79e072c5e4f039ce47f014042541b8fc0f4ba
alejandrojgb94 <55994125+alejandrojgb94@users.noreply.github.com>
2020-12-14 12:56:24 +0100

    Update monitor.cpp

--- src/monitor.cpp
+++ src/monitor.cpp
@@ -2,46 +2,65 @@
 #include <fstream>
 void Monitor::mostrarMonitor()
 {	
-	ifstream f;
-	string datoFich;
-	f.open("monitor.txt");
-	if(!f)
-		cout << "Error al abrir el fichero monitor" << endl;
-	else
-	{
-		datoFich = f.get();
-		while (!f.eof())
-		{
-			cout << datoFich << endl;
-			datoFich = f.get();
-		}
-		f.close();
+
+	ifstream archivo("monitor.txt");
+	if(!archivo.is_open()){
+		cout<<"Error al abrir el fichero de monitor.txt"<<endl;
 	}
 
-	/*cout << "Nombre: " << _nombre << endl;
-	cout << "DNI: " << _dni << endl;
-	cout << "Telefono: " << _telefono << endl;
-	cout << "ruta: " << _ruta << endl;*/
+	string nombre;
+	string pass;
+	string dni;
+	int telefono;
+	string ruta;
+
+    cout<< "| Nombre | \t | Pass | \t | Dni | \t | Telefono | \t | Ruta |" <<endl;
+    while (!archivo.eof()) {
+		archivo>>nombre;
+		archivo>>pass;
+		archivo>>dni;
+        archivo>>telefono;
+        archivo>>ruta;
+		cout << "|" <<nombre << "| \t |" <<pass << "| \t |" << dni << "| \t |" << telefono << "| \t |" << ruta << "|"<< endl;
+        archivo.ignore();
+	}
+	archivo.close(); //Cerramos el archivo.
 }
 
-void Monitor::registrarMonitor(string nombre, string dni, int telefono, string ruta)
+void Monitor::registrarMonitor(string nombreFichero)
 {
-	/*cout << "Introduza el nombre del monitor que desea registrar" << endl;
-	cin >> nombre;
-	cout << "Introduzca el dni del monitor" << endl;
-	cin >> dni;
-	cout << "Introduzca el teléfono del monitor" << endl;
-	cin >> telefono;
-	cout << "Introduzca la ruta que se le ha asociado al monitor" << endl;
-	cin >> ruta;*/
 
-	ofstream f;
-	f.open("monitor.txt");
-	if(!f)
-		cout << "Error al abrir fichero monitor" << endl;
-	else
-	{
-		f << nombre << " " << dni << " " << telefono << " " << ruta << endl;
-		f.close();
+    Monitor aux;
+    string nombre;
+    string pass;
+    string dni;
+    int telefono;
+    string ruta;
+
+    cout << "Introduce el nombre del monitor: " << endl;
+    cin >> nombre;
+
+    cout << "Introduce el password del monitor: " << endl;
+    cin >> pass;
+
+    cout << "Introduce el dni del mostrarMonitor: " << endl;
+    cin >> dni;
+
+    cout << "Introduce el telefono del monitor: " << endl;
+    cin >> telefono;
+
+    cout << "Introduce la ruta del monitor: " << endl;
+    cin >> ruta;
+
+
+
+    fstream archivo;
+    archivo.open(nombreFichero,fstream::app);
+
+	if(!archivo.is_open()){
+		cout<<"Error al abrir el fichero de monitor.txt"<<endl;
 	}
+
+    archivo<<nombre<<"\t"<<pass<<"\t"<<dni<<"\t"<<telefono<<"\t"<<ruta<<endl;
+    archivo.close();
 }
