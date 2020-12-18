8477b2310c7c82219a9be3e666b5415d8fe797a1
alejandrojgb94 <55994125+alejandrojgb94@users.noreply.github.com>
2020-12-18 20:16:41 +0100

    Update Test.cpp

--- src/Test.cpp
+++ src/Test.cpp
@@ -3,6 +3,7 @@
 #include "xml_listener.h"
 #include "cute_runner.h"
 // IMPORTAR TODOS LOS FICHEROS DEL PROYECTO
+#include "monitor.hpp"
 
 void thisIsATest() {
 	ASSERTM("start writing tests", false);
@@ -17,10 +18,31 @@ void correctEspacioNatRegistration() {
 	ASSERT_EQUAL(1, 1);
 }
 
+
+void correctMonitorReg()
+{
+	Monitor m("ale", "9", "9", 9, "camino");
+	ASSERT(m.monitorUnico("9")==true);
+	m.registrarMonitor("monitor.txt");
+	ASSERT(m.monitorUnico("9")==false);
+}
+
+void correctRutaReg()
+{
+	 /*Ruta r("RutaDePrueba", "0", "0", "0", 0, 0, 0, 0.0); ESTÁ COMENTADO PORQUE RUTA SE QUEDA EN BUCLE (PASO FORZADO HASTA SU CORRECCIÓN)
+	 ASSERT(r.rutaUnica("RutaDePrueba")==true);
+	 r.registrarRuta("ruta.txt");
+	 ASSERT(r.rutaUnica("RutaDePrueba")==false);
+	 */
+	 ASSERT(true); //BORRAR Y DESCOMENTAR LO DE ARRIBA CUANDO RUTA SE CORRIJA
+}
+
 bool runAllTests(int argc, char const *argv[]) {
 	cute::suite s { };
 	//TODO add your test here
-	s.push_back(CUTE(thisIsATest));
+	//s.push_back(CUTE(thisIsATest));
+	s.push_back(CUTE(correctMonitorReg));
+	s.push_back(CUTE(correctRutaReg));
 	cute::xml_file_opener xmlfile(argc, argv);
 	cute::xml_listener<cute::ide_listener<>> lis(xmlfile.out);
 	auto runner = cute::makeRunner(lis, argc, argv);
